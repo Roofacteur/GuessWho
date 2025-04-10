bcfb164e69a1700c3c7d635e6832e74df9051d46
Sacha Leone <Sacha.Leone@rpn.ch>
2025-04-10 11:32:54 +0200

    Diagramme de classe, planification et JT

---
+++ Projet/GuessWho/GuessWho/Program.cs
@@ -0,0 +1,23 @@
+﻿using Raylib_cs;
+using static Raylib_cs.Raylib;
+using System.Numerics;
+
+namespace GuessWho
+{
+    public class Program
+    {
+        static void Main() {
+            InitWindow(800, 600, "Hello Raylib!");
+            while (!WindowShouldClose())
+            {
+                BeginDrawing();
+                ClearBackground(Color.RayWhite);
+                EndDrawing();
+            }
+            CloseWindow();
+        }
+        
+    }
+       
+
+}
